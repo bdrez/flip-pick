6544358a88a4556dc900239c9ef7b9649566cb19
bdrez <159343619+bdrez@users.noreply.github.com>
2025-06-16 22:06:59 -0400

    Create formInput.js

---
+++ formInput.js
@@ -0,0 +1,5 @@
+var numPpl = document.getElementById("number");
+var gymDraft=document.getElementById(" ");
+var timeTurn=document.getElementById(" ");
+var timeReminder=document.getElementById(" ");
+var notiF=document.getElementById(" ");
