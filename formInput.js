1c1821307186403167594dc4e9888bcd1a134fd8
bdrez <159343619+bdrez@users.noreply.github.com>
2025-06-19 15:22:51 -0400

    Update formInput.js

--- formInput.js
+++ formInput.js
@@ -5,28 +5,41 @@ function test(){
     const gymDraft=document.getElementById("number2").value;
     const timeTurn=document.getElementById("time").value;
     const timeReminder=document.getElementById("time2").value;
-    const notiF=document.getElementById("dropdown").value;
+    const notiF=document.getElementById("dropdownDefaultButton").textContent;
 
     //alert(numPpl + gymDraft + timeTurn + timeReminder + notiF);
-
-    //storing data local
-    var ppl= localStorage.setItem("numPpl", numPpl);
-    var gym= localStorage.setItem("gymDraft", gymDraft);
-    var turn= localStorage.setItem("timeTurn", timeTurn);
-    var rem= localStorage.setItem("timeReminder", timeReminder);
-    var not= localStorage.setItem("notiF", notiF);
-
     //gonna generate a random code to be stored and shared
 
     const code =crypto.randomUUID().replaceAll("-", "");// generate a random uuid
 
+    const uuid=localStorage.setItem("uniqCode", code);
+
+    /*/storing data local
+    gonna make one whole thing 
+    const ppl= localStorage.setItem("numPpl", numPpl);
+    const gym= localStorage.setItem("gymDraft", gymDraft);
+    const turn= localStorage.setItem("timeTurn", timeTurn);
+    const rem= localStorage.setItem("timeReminder", timeReminder);
+    const not= localStorage.setItem("notiF", notiF);*/
+
+    const draftData={ //same name for short form
+        numPpl,
+        gymDraft,
+        timeTurn,
+        timeReminder,
+        notiF
+    };
+
+    localStorage.setItem(code, JSON.stringify(draftData));
+
+    window.location.href= 'codeEnterSheet.html?code=${code}';
+
     //to splie and get unique id to use as code for user to share
     //let uuid = crypto.randomUUID().replaceAll("-", "").slice(0, 12);
     //or make it a link they can share
-
-
-    var uniq=localStorage.setItem("uniqCode", uuid);
-
-
-
 }
+
+document.querySelector('form').addEventListener("submit", function(e){
+    e.preventDefault();
+    test();
+});
