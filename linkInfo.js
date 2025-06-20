b42ff36e17087f41483f8ca5dabf07269bd3df5d
bdrez <159343619+bdrez@users.noreply.github.com>
2025-06-20 16:43:03 -0400

    Update linkInfo.js

--- linkInfo.js
+++ linkInfo.js
@@ -1,2 +1,3 @@
 console.log(JSON.parse(window.localStorage.getItem('draftee')));
 //later potentially save all - this overwrite only stores current one
+const drat= JSON.parse(localStorage.getItem('person')) || [];
