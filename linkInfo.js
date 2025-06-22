9f580e7b52809ae935e55b1afaceb437c4722a52
bdrez <159343619+bdrez@users.noreply.github.com>
2025-06-22 00:10:08 -0400

    Update linkInfo.js

--- linkInfo.js
+++ linkInfo.js
@@ -12,3 +12,23 @@ const data=JSON.parse(localStorage.getItem("draftee"));
 if (data) {
   document.getElementById("codeDisplay").textContent = ` ${data.code}`;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const data = JSON.parse(localStorage.getItem("draftee"));
+  if (data) {
+    //only to display code 
+    document.getElementById("codeDisplay").textContent = ` ${data.code}`;
+
+
+    // 3. Do something like this:
+    const playerContainer = document.createElement("div");
+    for (let i = 0; i < Number(data.numPpl); i++) {
+      const playerBox = document.createElement("div");
+      playerBox.textContent = `Player ${i + 1}`;
+      playerContainer.appendChild(playerBox);
+    }
+    document.body.appendChild(playerContainer);
+
+   
+  }
+});
