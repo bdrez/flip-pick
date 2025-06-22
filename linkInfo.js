2ac57204e26ee7c72a87b4d2dafbe22f7dc06050
bdrez <159343619+bdrez@users.noreply.github.com>
2025-06-22 00:06:49 -0400

    Update linkInfo.js

--- linkInfo.js
+++ linkInfo.js
@@ -1,10 +1,14 @@
+/*onsole.log(JSON.parse(window.localStorage.getItem('draftee')));//to see in console
+const data=JSON.parse(localStorage.getItem('draftee'));
+//later potentially save all - this overwrite only stores current one
+const draftArray= JSON.parse(localStorage.getItem('person')) || [];
+const drafteeData=JSON.parse(localStorage.getItem("draftee"));
+draftArray.push(drafteeData);
+localStorage.setItem("person", JSON.stringify(draftArray));
+*/
 
 const data=JSON.parse(localStorage.getItem("draftee"));
+//only doing this for code bec it needs to be displayed
 if (data) {
   document.getElementById("codeDisplay").textContent = ` ${data.code}`;
-  document.getElementById("playersInfo").textContent = ` ${data.numPpl}`;
-  document.getElementById("timeTurnInfo").textContent = ` ${data.timeTurn}`;
-  document.getElementById("reminderInfo").textContent = ` ${data.timeReminder}`;
-  document.getElementById("notificationInfo").textContent = `Notify by: ${data.notiF}`;
-  
 }
